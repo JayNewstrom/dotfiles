5132395e1e5fe7e2d12b507554cde63f42720278
Jay Newstrom <jaynewstrom@gmail.com>
2020-01-15 10:39:21 -0600

    Properly quote things

--- dotfileInitialization.sh
+++ dotfileInitialization.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-gpg --list-keys || true;
-ln -sfn $(pwd)/.gnupg/gpg.conf $HOME/.gnupg/gpg.conf;
-ln -sfn $(pwd)/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf;
-ln -sfn $(pwd)/gitignore $HOME/.gitignore;
-ln -sfn $(pwd)/.gradle/gradle.properties $HOME/.gradle/gradle.properties;
+gpg --list-keys || true
+ln -sfn "$(pwd)/.gnupg/gpg.conf" "$HOME/.gnupg/gpg.conf"
+ln -sfn "$(pwd)/.gnupg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
+ln -sfn "$(pwd)/gitignore" "$HOME/.gitignore"
+ln -sfn "$(pwd)/.gradle/gradle.properties" "$HOME/.gradle/gradle.properties"
 
 for file in .{bash_profile,gitconfig,exports,aliases,android_bash_profile,path,inputrc}; do
-	ln -sfn "$(pwd)/$file" "$HOME/$file";
-done;
-unset file;
+  ln -sfn "$(pwd)/$file" "$HOME/$file"
+done
+unset file
